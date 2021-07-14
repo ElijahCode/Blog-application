55f9bc3f351791b92560767044fada9a99bc7fc3
Ilya <ilyastarickov1996@gmail.com>
2021-07-14 19:23:11 +0400

    feat: add eslint, clean comments

--- src/style-test/index.test.js
+++ src/style-test/index.test.js
@@ -1,25 +1,16 @@
-/* eslint-disable jest/no-jasmine-globals */
-/* globals jasmine */
-// https://www.npmjs.com/package/puppeteer-screenshot-tester
 import puppeteer from "puppeteer";
 import { toMatchImageSnapshot } from "jest-image-snapshot";
 
 expect.extend({ toMatchImageSnapshot });
 
-// import ScreenshotTester from "puppeteer-screenshot-tester";
-
 describe("styles.test", () => {
   let originalTimeout;
 
-  // extend default interval to 10s because some image processing might take some time
-  // we can do it beforeEach or once per test suite it's up to you
-  // if you're running that on fast computer/server probably won't need to do that
   beforeEach(() => {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   });
 
-  // set default interval timeout for jasmine
   afterEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
@@ -29,13 +20,9 @@ describe("styles.test", () => {
     { width: 600, height: 1080 },
   ].forEach(({ width, height }) =>
     it(`index page should have proper view for ${width}x${height} params`, async () => {
-      // setting up puppeteer
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      // set current view port size
       await page.setViewport({ width, height });
-      // navigate to the page, served with webpack
-      // IMPORTANT!: test assumes webpack is started
 
       let image;
 
